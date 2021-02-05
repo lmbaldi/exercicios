42e03455279e4a135cae993bc6f6b733d8d248b7
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-05 09:58:21 -0300

    add class AlterarNomePessoa

---
+++ src/jdbc/ConsultarPessoas2.java
@@ -0,0 +1,45 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsultarPessoas2 {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Scanner entrada = new Scanner(System.in);
+
+		Connection conexao = FabricaConexao.getConexao();
+		String sql = "SELECT * FROM pessoas WHERE nome like ?";
+		
+		System.out.println("Informe o nome para a pesquisa: ");
+		String nomePesquisado = entrada.nextLine();
+		
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, "%" + nomePesquisado + "%");
+		ResultSet resultado = stmt.executeQuery();
+
+		List<Pessoa> pessoas = new ArrayList<>();
+
+		while (resultado.next()) {
+			int codigo = resultado.getInt("codigo");
+			String nome = resultado.getString("nome");
+			pessoas.add(new Pessoa(codigo, nome));
+		}
+
+		for (Pessoa p : pessoas) {
+			System.out.println(p.getCodigo() + " ==> " + p.getNome());
+		}
+
+		stmt.close();
+		conexao.close();
+		entrada.close();
+	}
+
+}
